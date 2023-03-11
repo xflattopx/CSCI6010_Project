b33bb0bb0909209cc8f5330a7e3accbd1d90ed8d
chasemoore <xflattopx@gmail.com>
2023-03-11 14:34:28 -0500

    made a successful request to airnows api

--- services/rest.py
+++ services/rest.py
@@ -30,9 +30,28 @@ def getPurpleAirSensorData(sensor_index, api_key):
         data = response.json()
         return data['sensor']['stats']['pm2.5']
 
+def getAirNowSensorData(api_key):
+
+    # specify the latitude and longitude of Raleigh, NC
+    lat = 35.7796
+    lon = -78.6382
+
+    # make the API request
+    url = f'https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude={lat}&longitude={lon}&distance=25&API_KEY={api_key}&parameter=PM2.5'
+    response = requests.get(url)
+    if response.status_code == 200:
+        # parse the JSON response
+        data = response.json()
+        # print the data
+        print(data)
+    
+
 
 ##  MAIN
-api_key = "B276397E-A658-11ED-B6F4-42010A800007"
+api_key_purple = "B276397E-A658-11ED-B6F4-42010A800007"
+api_key_air = "625E165E-03E8-4317-B7F7-1BDB0290A448"
+getAirNowSensorData(api_key_air)
+
 # ADB_This will be an input on website rather than a dictionary.  For now it will be hard coded
 inputDictionaryADB = {"NASA_AQCS_45":104950, "NASA_AQCS_36":47535, "NASA_AQCS_21":47497, "NASA_AQCS_87":47633,
                       "NASA_AQCS_17":47489, "NASA_AQCS_12":47479, "NASA_AQCS_19":47493, "NASA_AQCS_7":47469,
@@ -42,13 +61,13 @@ input_sensors = inputData(inputDictionaryADB)
 # Retrieve sensor data and store it in a dataframe
 sensor_data = []
 for sensor in input_sensors:
-    pm25_value = getPurpleAirSensorData(sensor, api_key)
+    pm25_value = getPurpleAirSensorData(sensor, api_key_purple)
     sensor_data.append({'Sensor ID': sensor, 'PM2.5 Value': pm25_value})
 
 df = pd.DataFrame(sensor_data)
 print(df)
 # Display the data using a plotly scatter plot
-fig = px.scatter(df, x='Sensor ID', y='PM2.5 Value', title='PurpleAir Sensor Data')
+fig = px.scatter(df, x='Sensor ID', y='PM2.5 Value', title='PurpleAir PM 2.5 Sensor Data')
 fig.show()
 
 
