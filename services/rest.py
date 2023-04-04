f89d5ccefe84615960976be7bfb0a36f745ab2f8
Your Name <deniseb0711@gmail.com>
2023-04-03 21:52:12 -0400

    Added historical end point

--- services/rest.py
+++ services/rest.py
@@ -6,6 +6,8 @@
 ## Sensor List: 
 import requests
 
+#Todo: Add in call to Open AQ for sensor list
+
 def inputData(inputDictionaryADB):
     sensor_index = list(inputDictionaryADB.values())
     return sensor_index
@@ -34,8 +36,7 @@ def getPurpleAirSensorData(sensor_index, api_key):
 
 def getAllPurpleAirSensorData(api_key,input):
     # Define the API endpoint for retrieving sensor metadata
-    metadata_url = 'https://api.purpleair.com/v1/sensors?location=Raleigh,NC&location_type=0&fields=sensor_index,name,latitude,longitude,pm2.5'
-
+    metadata_url = "https://api.purpleair.com/v1/sensors/47535/history?start_timestamp=1646189608&fields=humidity%2Cpm2.5_atm"
     headers = {
         "X-API-Key": api_key
     }
@@ -59,6 +60,7 @@ def getAllPurpleAirSensorData(api_key,input):
 
 ##  MAIN
 api_key = "B276397E-A658-11ED-B6F4-42010A800007"
+
 # ADB_This will be an input on website rather than a dictionary.  For now it will be hard coded
 inputDictionaryADB = {"NASA_AQCS_45":104950, "NASA_AQCS_36":47535, "NASA_AQCS_21":47497, "NASA_AQCS_87":47633,
  "NASA_AQCS_17":47489, "NASA_AQCS_12":47479, "NASA_AQCS_19":47493, "NASA_AQCS_7":47469, "NASA_AQCS_91":29257, "NASA_AQCS_55":47573}
