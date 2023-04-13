fb351a200d97e5f0ee3241e6786ae7817f6c4216
Your Name <deniseb0711@gmail.com>
2023-04-12 21:38:30 -0400

    plots

--- services/rest.py
+++ services/rest.py
@@ -141,13 +141,14 @@ def getAirNowSensorData(api_key):
     lon = -78.6382
 
     # make the API request
-    url = f'https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude={lat}&longitude={lon}&distance=25&API_KEY={api_key}&parameter=PM2.5'
-    url2 = f'https://www.airnowapi.org/aq/data/?startDate=2023-03-02T00&endDate=2023-03-22T01&parameters=PM25&BBOX=-78.931785,35.588992,-78.214927,36.052221&dataType=B&format=application/json&verbose=1&monitorType=0&includerawconcentrations=1&API_KEY=' + api_key
+    url = 'https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude={lat}&longitude={lon}&distance=25&API_KEY={api_key}&parameter=PM2.5'
+    url2 = 'https://www.airnowapi.org/aq/data/?startDate=2022-01-01T00&endDate=2022-12-31T01&parameters=PM25&BBOX=-78.931785,35.588992,-78.214927,36.052221&dataType=B&format=application/json&verbose=1&monitorType=0&includerawconcentrations=1&API_KEY=' + api_key
     response = requests.get(url2)
     if response.status_code == 200:
         # parse the JSON response
+        data_an = response.json()
         #print (data)
-        print(data)
+        print(data_an)
 
     
 
@@ -160,7 +161,7 @@ api_key_air = "625E165E-03E8-4317-B7F7-1BDB0290A448"
 
 #sensor = 104950
 # ADB_This will be an input on website rather than a dictionary.  For now it will be hard coded
-inputDictionaryADB = {"NASA_AQCS_45":104950 ,"NASA_AQCS_36":47535} #"NASA_AQCS_21":47497, "NASA_AQCS_87":47633,
+inputDictionaryADB = {"NASA_AQCS_45":104950} #"NASA_AQCS_36":47535 #"NASA_AQCS_21":47497, "NASA_AQCS_87":47633,
                     #    "NASA_AQCS_17":47489, "NASA_AQCS_12":47479, "NASA_AQCS_19":47493, "NASA_AQCS_7":47469,
                     #    "NASA_AQCS_91":29257, "NASA_AQCS_55":47573}
 input_sensors = inputData(inputDictionaryADB)
@@ -181,10 +182,18 @@ for sensor in input_sensors:
     data2 =data['data']
     df = pd.DataFrame(data2, columns= ['Time', "Humidity", "PM25"])
     df["Time"] = df["Time"].apply(convert_timestamp_to_est)
-    df.sort_values('PM25')  
+    df[['Date', 'Time1']] = df['Time'].str.split(' ', expand= True)
+
+    df = df.sort_values('Date')  
     print(df)
-    fig = px.scatter(df, x='Time' , y='PM25', title= str(sensor)+ ' PurpleAir PM 2.5 Sensor Data')
+    fig = px.line(df, x='Date' , y='PM25', title= str(sensor)+ ' PurpleAir PM 2.5 Sensor Data')
+    fig2 = px.scatter(df, x='Date' , y='PM25', title= str(sensor)+ ' PurpleAir PM 2.5 Sensor Data')
+
+    #fig.update_xaxes(range=[0, 366], tickmode='array', tickvals=[2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56, 59, 62, 65, 68, 71, 74, 77, 80, 83, 86, 89, 92, 95, 98, 101, 104, 107, 110, 113, 116, 119, 122, 125, 128, 131, 134, 137, 140, 143, 146, 149, 152, 155, 158, 161, 164, 167, 170, 173, 176, 179, 182, 185, 188, 191, 194, 197, 200, 203, 206, 209, 212, 215, 218, 221, 224, 227, 230, 233, 236, 239, 242, 245, 248, 251, 254, 257, 260, 263, 266, 269, 272, 275, 278, 281, 284, 287, 290, 293, 296, 299, 302, 305, 308, 311, 314, 317, 320, 323, 326, 329, 332, 335, 338, 341, 344, 347, 350, 353, 356, 359, 362, 365])
     fig.show()
+    fig2.show()
+
+    print(df.head(365)) 
 
 
             #if (len(pm25_value[1])!= 0):
