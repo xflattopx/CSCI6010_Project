dc5c5a5bd8d69fb5c491f5bb9de6d1b74b684c19
Your Name <deniseb0711@gmail.com>
2023-04-15 16:10:04 -0400

    completey raw data output from AirNow

--- services/test.py
+++ services/test.py
@@ -6,9 +6,7 @@ from datetime import datetime, timedelta
 import json 
 
 def getAirNowSensorData(api_key):
-    #specify the latitude and longitude of Raleigh, NC
-    lat = 35.7796
-    lon = -78.6382
+    #
 
     url = 'https://www.airnowapi.org/aq/data/?startDate=2023-03-3T18&endDate=2023-04-01T19&parameters=PM25&BBOX=-78.647643,35.731814,-78.460876,35.941117&dataType=A&format=application/json&verbose=1&monitorType=0&includerawconcentrations=1&API_KEY='+ str(api_key)     # make the API request
     
@@ -19,21 +17,19 @@ def getAirNowSensorData(api_key):
 #         # parse the JSON response
         data_an = response.json()
         df = pd.json_normalize(data_an)
-        print (df['RawConcentration'])
-        df = df.drop(df[df['RawConcentration'] == 'PM2.5'].index)
-        df[['RawConcentration']] = df[['RawConcentration']].apply(pd.to_numeric)
         
-        df['UTC'] = pd.to_datetime(df['UTC'])
 
-# set the UTC column as the dataframe index
-        df = df.set_index('UTC')
-      
+# reset the index to make the "UTC" column a regular column again
+        df = df.reset_index()
 
+# drop the rows with missing values (i.e. the first 23 rows)
+        df = df.dropna()
 # resample the dataframe by 24 hour intervals and compute the average of PM2.5 values
         
-#         
+        print(df)
         
 
 airnow_api =  '1A90A9DF-368D-45C1-BF6B-14701DC449D1'
 
 getAirNowSensorData(airnow_api)
+
